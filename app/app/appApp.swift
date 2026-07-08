4d8d32046a841fedadbccfdeadbcdaf3759d2419
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-07-07 22:00:48 -0700

    first commit

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Mahasvin Shanmugapriya Manikandan on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
