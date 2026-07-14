d7d5ad40eda33cf4e41b036ba7b25601894942c3
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-07-13 17:21:47 -0700

    request json encoding

--- max-app/max-app/APIClient.swift
+++ max-app/max-app/APIClient.swift
@@ -12,9 +12,22 @@ struct APIClient {
     // generic api request
     func request<Input: Encodable, Output: Decodable>(path: String, action: String, body: Input?) async throws -> Output {
         
+        // url
         guard let url = URL(string: "\(Constants.baseURL)\(path)") else {
             throw APIError.invalidURL
         }
         
+        // create request
+        var req = URLRequest(url: url)
+        req.httpMethod = action
+        
+        if let body {
+            let encoder = JSONEncoder()
+            encoder.keyEncodingStrategy = .convertToSnakeCase
+            req.httpBody = try encoder.encode(body)
+            req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+        
+        let d
     }
 }
