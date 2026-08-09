060d3b140c156bb0b16e04696826e85222e9b835
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-08-08 21:00:37 -0700

    changed conversation to optional field

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -31,11 +31,12 @@ def get_messages_for_conversation(conversation_id: int):
 @router.post("/messages", response_model=MessageResponse)
 def message_agent(message: Message):
     # Create new conversation conversation_id == -1
-    conv_id = message.conversation.conversation_id
-    if conv_id == -1:
+    is_new = message.conversation is None
+    if is_new:
         convo = db.create_conversation(message.content)
         message.conversation = Conversation(**convo)
-        conv_id = message.conversation.conversation_id
+
+    conv_id = message.conversation.conversation_id
 
     # add user message to table
     db.insert_message(conv_id, "user", message.content)
@@ -49,7 +50,7 @@ def message_agent(message: Message):
     obj = db.insert_message(conv_id, "assistant", content)
 
     # create title for new conversations
-    if conv_id == -1:
+    if is_new:
         messages = db.get_messages_for_id(conv_id)
         title = agent.quick_message(messages=messages, sys_prompt=config.TITLE_GEN_PROMPT)
         db.update_conversation_title(title)
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Conversation(BaseModel):
     conversation_id: int
@@ -11,8 +12,8 @@ class ConversationList(BaseModel):
     count: int
 
 class Message(BaseModel):
-    conversation: Conversation
     content: str
+    conversation: Optional[Conversation] = None
 
 class MessageResponse(BaseModel):
     conversation_id: int
