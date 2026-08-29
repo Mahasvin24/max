317799b95ae358c1f04a69b99f12a23f3160f4da
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-08-28 22:19:31 -0700

    thinking messages

--- backend/agent.py
+++ backend/agent.py
@@ -12,7 +12,11 @@ def quick_message(messages: list[Message], sys_prompt: str = config.SYSTEM_PROMP
     sys_msg = Message(role="system", content=sys_prompt)
     messages.insert(0, sys_msg)
 
-    return ollama.chat(model=config.MODEL, messages=messages)
+    return ollama.chat(model=config.MODEL, messages=messages, think=False)
 
 def thinking_message(messages: list[Message], sys_prompt: str | None = None):
-    ...
+    # System prompt
+    sys_msg = Message(role="system", content=sys_prompt)
+    messages.insert(0, sys_msg)
+    
+    return ollama.chat(model=config.MODEL, messages=messages, think=False)
