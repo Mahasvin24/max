92f2436524e72a3126599ceb328845341b1c6bb6
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-09-20 21:46:20 -0700

    addressed path ambiguity in config file

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,17 @@
+# config.py 
 # Global variables
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent # aka, backend folder
+PROMPTS_DIR = BASE_DIR / "prompts"
+
+DATABASE = BASE_DIR / "database" / "data.db"
 
 MODEL = "openai/gpt-oss-120b"
 
-DATABASE = "database/data.db"
-
-with open("prompts/SYSTEM_PROMPT.md", "r") as f:
+with open(PROMPTS_DIR / "SYSTEM_PROMPT.md", "r") as f:
     SYSTEM_PROMPT = f.read()
 
-with open("prompts/TITLE_GEN_PROMPT.md", "r") as f:
+with open(PROMPTS_DIR / "TITLE_GEN_PROMPT.md", "r") as f:
     TITLE_GEN_PROMPT = f.read()
